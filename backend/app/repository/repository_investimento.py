53ea3ab5ce9bf49076b632d4037aa265d08ad19a
JonatasHCR <jonatas.reisprofissional@gmail.com>
2025-04-17 17:57:59 -0300

    :sparkles: feat: Criação da tabela de investimentos

---
+++ backend/app/repository/repository_investimento.py
@@ -0,0 +1,52 @@
+from os import getenv
+
+import psycopg2
+from dotenv import load_dotenv
+
+from .repository import Repository
+
+load_dotenv()
+
+
+class RepositoryInvestimento(Repository):
+    def __init__(self):
+        self.host = getenv("HOST")
+        self.database = getenv("DATABASE")
+        self.user = getenv("USER")
+        self.password = getenv("PASSWORD")
+
+        self.criar_tabela()
+
+    def connectar(self):
+        self.conenection = psycopg2.connect(
+            host=self.host,
+            database=self.database,
+            user=self.user,
+            password=self.password,
+        )
+        self.cursor = self.conenection.cursor()
+
+    def desconectar(self):
+        self.cursor.close()
+        self.conenection.close()
+
+    def criar_tabela(self):
+        try:
+            self.connectar()
+
+            query = """
+            CREATE TABLE IF NOT EXISTS investimentos (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(25) NOT NULL,
+                descricao TEXT,
+                tipo CHAR(1) NOT NULL,
+                valor REAL NOT NULL,
+                data DATE NOT NULL
+            );
+            """
+
+            self.cursor.execute(query)
+            self.conenection.commit()
+
+        finally:
+            self.desconectar()
--- backend/tests/test_repository_investimento.py
+++ backend/tests/test_repository_investimento.py
@@ -3,6 +3,7 @@ from os import getenv
 from psycopg2 import connect
 from dotenv import load_dotenv
 
+from app.repository.repository_investimento import RepositoryInvestimento
 
 load_dotenv()
 
