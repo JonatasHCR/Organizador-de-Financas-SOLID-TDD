4754bcaadd4c7fd11c532fa79e1fe51d2b3381ca
JonatasHCR <jonatas.reisprofissional@gmail.com>
2025-04-17 17:45:51 -0300

    :test_tube: test: Teste criação da tabela investimentos

---
+++ backend/tests/test_repository_investimento.py
@@ -0,0 +1,41 @@
+from os import getenv
+
+from psycopg2 import connect
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class TestRepositoryAtivo:
+    def test_criacao_tabela_ativos(self):
+        try:
+            repository = RepositoryInvestimento()
+
+            conenection = connect(
+                host=getenv("HOST"),
+                database=getenv("DATABASE"),
+                user=getenv("USER"),
+                password=getenv("PASSWORD"),
+            )
+            cursor = conenection.cursor()
+
+            query = """
+            SELECT 1 
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+            AND table_name = 'investimentos';
+            """
+
+            cursor.execute(query)
+            existe = cursor.fetchone()
+            if existe:
+                assert True
+            else:
+                assert False
+
+        except Exception as error:
+            print(error)
+            assert False
+        finally:
+            cursor.close()
