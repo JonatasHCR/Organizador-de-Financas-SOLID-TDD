aeca1529d611e6d7f6a6a5c04e6186a8d8d07c5b
JonatasHCR <jonatas.reisprofissional@gmail.com>
2025-04-17 15:55:38 -0300

    :test_tube: test: Criando novo teste modelo passivo

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -26,3 +26,28 @@ class TestModels:
         except Exception as e:
             print(e)
             assert False
+
+    def test_model_passivo(self):
+        id_teste = 1
+        nome_teste = "despesa"
+        descricao_teste = "teste"
+        valor_teste = 50.0
+        data_teste = date.today()
+        fixo_teste = "N"
+        vencimento_teste = date.today()
+        plano_pagamento_teste = "M"
+        try:
+            modelo_teste = ModelPassivo(
+                id=id_teste,
+                nome=nome_teste,
+                descricao=descricao_teste,
+                valor=valor_teste,
+                data_pagamento=data_teste,
+                fixo=fixo_teste,
+                vencimento=vencimento_teste,
+                plano_pagamento=plano_pagamento_teste,
+            )
+            assert True
+        except Exception as e:
+            print(e)
+            assert False
