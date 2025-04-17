37b1072d0e653ae0a6f093b6bf2bd05bf1fd8c16
JonatasHCR <jonatas.reisprofissional@gmail.com>
2025-04-17 19:18:28 -0300

    :construction: Em progresso

--- backend/tests/models_test/test_models.py
+++ backend/tests/models_test/test_models.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0,PROJECT_ROOT)
 from datetime import date
 
 from app.models.model_ativo import ModelAtivo
