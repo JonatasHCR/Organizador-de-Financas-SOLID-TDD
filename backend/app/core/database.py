1fc9fa8287e12a2903238348015a296ba10a0a89
JonatasHCR <jonatas.reisprofissional@gmail.com>
2025-07-15 18:06:59 -0300

    :recycle: refactor: Mudando nome e forma de configuração

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,16 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.settings import SettingsProducao
+from app.core.settings import Settings
 
 
-DATABASE_URL = SettingsProducao().DATABASE_URL
+DATABASE_URL = Settings().DATABASE_URL
 
 
-class Database:
-    Base = declarative_base()
+Base = declarative_base()
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-    def __init__(self, database_url: str = DATABASE_URL):
-        self.engine = create_async_engine(database_url, echo=True)
-        self.SessionLocal = sessionmaker(
-            bind=self.engine, class_=AsyncSession, expire_on_commit=False
-        )
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -5,22 +5,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = dirname(dirname(dirname(__file__)))
 
 
-class SettingsBase(BaseSettings):
+class Settings(BaseSettings):
     DATABASE_URL: str
-
-
-class SettingsProducao(SettingsBase):
     ENV: str = "producao"
-
-    model_config = SettingsConfigDict(
-        env_file=join(BASE_DIR, f".env.{ENV}"),
-        env_file_encoding="utf-8",
-        extra="ignore",
-    )
-
-
-class SettingsTeste(SettingsBase):
-    ENV: str = "teste"
 
     model_config = SettingsConfigDict(
         env_file=join(BASE_DIR, f".env.{ENV}"),
