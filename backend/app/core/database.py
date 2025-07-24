08f84d0fdfcb35e28ebaf26ce453b67a03cd585d
JonatasHCR <jonatas.reisprofissional@gmail.com>
2025-07-24 16:21:07 -0300

    :recycle: refactor: formatando PEP8

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
+
 async def get_db():
     async with SessionLocal() as session:
         yield session
--- backend/app/schema/investimento.py
+++ backend/app/schema/investimento.py
@@ -13,7 +13,9 @@ class InvestimentoSchema(BaseModel):
         ..., description="Tipo de investimento"
     )
     valor: float = Field(..., ge=0, description="Valor do investimento")
-    data_adquirido: date = Field(..., description="Data que foi adquirido o investimento")
+    data_adquirido: date = Field(
+        ..., description="Data que foi adquirido o investimento"
+    )
     conta_id: int = Field(..., gt=0, description="Conta que está relacionado")
 
     model_config = ConfigDict(from_attributes=True)
@@ -26,6 +28,6 @@ class InvestimentoOutputSchema(InvestimentoSchema):
 class InvestimentoResponseSchema(BaseModel):
     status: str = Field(..., description="Status da resposta")
     investimento: InvestimentoOutputSchema = Field(..., description="Investimento")
-    data_hora: datetime = Field(...,description="Data e hora da resposta")
+    data_hora: datetime = Field(..., description="Data e hora da resposta")
 
     model_config = ConfigDict(from_attributes=True)
